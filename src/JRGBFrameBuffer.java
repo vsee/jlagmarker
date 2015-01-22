7c236f45b011d5aaaff3d345bb0202add40f0820
Volker Seeker <s1160444@sms.ed.ac.uk>
2015-01-22 12:32:09 +0000

    Read and write byte buffer methods added

---
+++ src/JRGBFrameBuffer.java
@@ -0,0 +1,27 @@
+public class JRGBFrameBuffer {
+	public static final int CHANNEL_NUM = 3;
+	
+	private int width;
+	private int height;
+	private int buffSize;
+	
+	/** Treat this as unsigned byte by interpreting with: b & 0xFF */
+	private byte[] buffer;
+	
+	public int getWidth() { return width; }
+	public int getHeight() { return height; }
+	public int getSize() { return buffSize; }
+	
+	public int getRawChannel(int idx) {
+		return buffer[idx] & 0xFF;
+	}
+	
+	public int getChannel(int pixel, int channel) {
+		if (channel > 2 || channel < 0)
+			throw new RuntimeException("Given channel must be between 0 and " + (CHANNEL_NUM - 1) + "!");
+		
+		int idx = pixel * CHANNEL_NUM + channel;
+		
+		return buffer[idx] & 0xFF;
+	}
+}
--- src/LagmarkerNative.java
+++ src/LagmarkerNative.java
@@ -1,14 +1,30 @@
 public class LagmarkerNative {
-	
 	static {
 		System.loadLibrary("LagmarkerNative");
 	}
 	
 	private native int lnativeEcho(int x);
 	
+	private native boolean lnativeLoadRGBFrameBuffer(String filename, JRGBFrameBuffer targetBuff);
+	
+	private native boolean lnativeSaveRGBFrameBuffer(String filename, JRGBFrameBuffer sourceBuff); 
+	
 	public static void main(String[] args) {
-		new HelloJNI().sayHello();
-		System.out.println(new LagmarkerNative().lnativeEcho(1904));
+		LagmarkerNative lnative = new LagmarkerNative();
+		JRGBFrameBuffer buff = new JRGBFrameBuffer();
+		if(!lnative.lnativeLoadRGBFrameBuffer("res/bw_frame.ppm", buff)) {
+			System.err.println("ERROR: Reading RGB buffer failed!");
+			return;
+		}
+
+		System.out.println("Reading RGB buffer successfull: " 
+				+ buff.getWidth() + "x" + buff.getHeight() 
+				+ " size: " + buff.getSize());
+		
+		if(!lnative.lnativeSaveRGBFrameBuffer("res/jbw_frame.ppm", buff)) {
+			System.err.println("ERROR: Saving RGB buffer failed!");
+			return;
+		}
 	}
 
 }
