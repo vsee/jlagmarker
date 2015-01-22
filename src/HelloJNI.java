bdf860423307a1a28bec60ceec5ecc751abba858
Volker Seeker <s1160444@sms.ed.ac.uk>
2015-01-22 07:43:42 +0000

    initial commit

---
+++ src/HelloJNI.java
@@ -0,0 +1,15 @@
+public class HelloJNI {
+	
+	static {
+		System.loadLibrary("hello");
+	}
+	
+	private native void sayHello();
+	
+	public static void main(String[] args) {
+		
+		new HelloJNI().sayHello();
+
+	}
+
+}
